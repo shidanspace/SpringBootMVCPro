e940c032dff05a90fe457718a91411eb2b9d02d7
F181200097 <shidan_space@126.com>
2020-07-18 11:35:59 +0800

    init

---
+++ src/main/java/com/shi/studyboot/StudybootApplication.java
@@ -0,0 +1,13 @@
+package com.shi.studyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shi/studyboot/StudybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shi.studyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
