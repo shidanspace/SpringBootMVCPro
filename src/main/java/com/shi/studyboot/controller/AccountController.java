f26c56345c01ea24df5c863f38b62785903956a9
F181200097 <shidan_space@126.com>
2020-08-01 21:56:04 +0800

    add css js images

---
+++ src/main/java/com/shi/studyboot/controller/AccountController.java
@@ -0,0 +1,17 @@
+package com.shi.studyboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+
+    @RequestMapping("/login")
+    public String login() {
+
+        return "account/login";
+
+    }
+}
